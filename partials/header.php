c992c988eda9f37fdc95090e25a8e5cc25ffd8ac
Elsa Rahel Wambena <elsa.wambena@pertamina.com>
2021-11-17 20:32:09 +0700

    adding folder partials

---
+++ partials/header.php
@@ -0,0 +1,8 @@
+<?php
+
+// header
+echo "<h1>SaCode</h1>";
+echo "<p>#LearnToCode</p>";
+echo "<hr>";
+
+?>
--- profile.php
+++ profile.php
@@ -1,24 +1,15 @@
 <?php
 
-// header
-echo "<h1>SaCode</h1>";
-echo "<p>#LearnToCode</p>";
-echo "<hr>";
+include('partials/header.php');
+include('partials/nav.php');
 
-// navigasi
-echo "<a href='index.php'>Home</a> ";
-echo "<a href='profile.php'>Profile</a> ";
-echo "<a href='gallery.php'>Gallery</a> ";
-echo "<a href='shop.php'>Shop</a> ";
-echo "<a href='contact.php'>Contact</a> ";
 
 // main content
 echo "<h2>Profile Page</h2>";
 echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ipsa reiciendis deleniti accusamus, doloribus dolorem perspiciatis modi impedit corrupti natus ducimus eos quia vel architecto quo nostrum aperiam excepturi nemo.</p>";
 
-
-// footer
-echo "<small>Copyright &copy; 2021</small>";
+include('partials/footer.php');
 
 ?>
+
 
